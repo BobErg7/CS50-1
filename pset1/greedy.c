1702c88e7f371c2f792234d8b93e156031e32ec3
Sean McNamara <admin@coder.ie>
2016-03-28 21:43:37 +0000

    Version 2 of greedy, now with modulus, faster with bigger numbers

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -16,29 +16,28 @@ int main(void)
         input = GetFloat();
     } while(input < 0);
     
-    int c = 0;
-    
+    // Fix input to integer so we don't have to deal with weird float math
     float toCents = round(input * 100);
-    
     int cents = toCents;
     
+    // Break change into least amount of coins possible
+    int c = 0;
     do {
-        
-        if(cents - 25 >= 0) {
-            cents = cents - 25;
-            c++;
+        if(cents % 25 < cents) {
+            c = c + round(cents/25);
+            cents = cents % 25;
         } 
-        else if(cents - 10 >= 0) {
-            cents = cents - 10;
-            c++;    
-        } 
-        else if (cents - 5 >= 0) {
-            cents = cents - 5;
-            c++;
+        else if(cents % 10 < cents) {
+            c = c + round(cents/10);
+            cents = cents % 10;
         }
-        else if(cents - 1 >= 0) {
-            cents = cents - 1;
-            c++;
+        else if(cents % 5 < cents) {
+            c = c + round(cents/5);
+            cents = cents % 5;
+        }
+        else{
+            c = c + round(cents/1);
+            cents = cents % 1;
         }
     } while(cents > 0);
     
