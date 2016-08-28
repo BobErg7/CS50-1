74c869936e75d307bb2246716709d57bcf8dd504
Sean McNamara <admin@coder.ie>
2016-08-28 13:03:26 +0000

    Linear search in C

---
+++ pset3/find/generate.c
@@ -0,0 +1,55 @@
+/**
+ * generate.c
+ *
+ * Computer Science 50
+ * Problem Set 3
+ *
+ * Generates pseudorandom numbers in [0,LIMIT), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// constant
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // Check to make sure the right amount of arguments are passed. (2 or 3)
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: generate n [s]\n");
+        return 1;
+    }
+
+    // Convert the argument from a string to integer
+    int n = atoi(argv[1]);
+
+    // If the seed argument is passed, send it to srand48, otherwise use time()
+    if (argc == 3)
+    {
+        srand48((long int) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long int) time(NULL));
+    }
+
+    // Loop through the amount of numbers requested printing generated random numbers.
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
