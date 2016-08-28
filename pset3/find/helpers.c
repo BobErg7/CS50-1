60dd483a3e143356182f533fcc704e632141f953
Sean McNamara <admin@coder.ie>
2016-08-28 13:42:10 +0000

    Insertion sort array

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -30,6 +30,27 @@ bool search(int value, int values[], int n)
  */
 void sort(int values[], int n)
 {
-    // TODO: implement an O(n^2) sorting algorithm
+    // Sort the array using insertion sort..
+    for(int i = 1; i < n; i++) {
+        int current = values[i];
+        int newKey = i;
+        while(newKey > 0 && values[newKey - 1] > current) {
+            values[newKey] = values[newKey - 1];
+            newKey = newKey - 1;
+        }
+        values[newKey] = current;
+    }
+    
     return;
 }
+
+
+
+
+
+
+
+
+
+
+
